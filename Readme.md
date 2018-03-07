56d1e2d1e095dcab2b789f5b38250b101864860b
Ryan Smith <rms1000watt@gmail.com>
2018-03-07 08:14:53 -0800

    init commit

---
+++ Readme.md
@@ -0,0 +1,21 @@
+# Homebrew Veritone
+
+## Introduction
+
+Homebrew taps for Veritone tools
+
+## Contents
+
+- [Taps](#taps)
+- [Usage](#usage)
+
+## Taps
+
+- [tagger: Tag Git/Github repos and Docker images with the same value](https://github.com/veritone/tagger)
+
+## Usage
+
+```bash
+brew tap veritone/veritone
+brew install tagger
+```
---
+++ tagger.rb
@@ -0,0 +1,19 @@
+require "FileUtils"
+
+class tagger < Formula
+  homepage "https://github.com/veritone/tagger"
+
+  v = "1.0.0"
+  url "https://github.com/veritone/tagger/releases/download/#{v}/tagger-darwin-amd64"
+  version v
+  sha256 "4af09eTODO:CHANGE_ME0b354a96b8"
+
+  def install
+    FileUtils.mv("tagger-darwin-amd64", "tagger")
+    bin.install "tagger"
+  end
+
+  test do
+    system "#{bin}/tagger", "version"
+  end
+end
