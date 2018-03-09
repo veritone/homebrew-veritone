a800120847d8a61ba7070ecc4351527904fe7c0a
Ryan Smith <rms1000watt@gmail.com>
2018-03-09 07:53:35 -0800

    bumped tagger

--- tagger.rb
+++ tagger.rb
@@ -3,10 +3,10 @@ require "FileUtils"
 class Tagger < Formula
   homepage "https://github.com/veritone/tagger"
 
-  v = "1.2.0"
+  v = "1.2.1"
   url "https://github.com/veritone/tagger/releases/download/#{v}/tagger-darwin-amd64"
   version v
-  sha256 "b74bcceadb9ad22bb67dead0a0ffafd95a553fc9796c3099079c01d29b740a5c"
+  sha256 "a67a2699d23b2a93dc9af4e930a9ee9912d4d7d3566ba16a17dd511594d05a15"
 
   def install
     FileUtils.mv("tagger-darwin-amd64", "tagger")
