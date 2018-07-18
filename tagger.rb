5e1abd0f7c945d0ee4ef9fc17356f7b30f74f2b3
Ryan Smith <rms1000watt@gmail.com>
2018-07-18 16:30:14 -0700

    updated tagger version

--- tagger.rb
+++ tagger.rb
@@ -6,10 +6,10 @@ require "FileUtils"
 class Tagger < Formula
   homepage "https://github.com/veritone/tagger"
 
-  v = "1.2.1"
+  v = "1.2.2"
   url "https://github.com/veritone/tagger/releases/download/#{v}/tagger-darwin-amd64"
   version v
-  sha256 "a67a2699d23b2a93dc9af4e930a9ee9912d4d7d3566ba16a17dd511594d05a15"
+  sha256 "0ddd90fcc149b5e8b3528ee2c606ef0294026683b267662a56234809faf99f34"
 
   def install
     FileUtils.mv("tagger-darwin-amd64", "tagger")
